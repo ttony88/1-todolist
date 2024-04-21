7906d2aae5747df402b34d2400d54275884312b4
ttony62 <1988anton62@gmail.com>
2024-04-21 19:27:52 +0300

    add middleware

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,10 +1,19 @@
-import { combineReducers, createStore } from "redux";
+import { AnyAction, applyMiddleware, combineReducers, legacy_createStore } from "redux";
 import { tasksReducer } from "./tasks-reducer";
 import { todolistsReducer } from "./todolists-reducer";
+import { ThunkDispatch, thunk } from "redux-thunk";
+import { TypedUseSelectorHook, useDispatch, useSelector } from "react-redux";
 
 const rootReducer = combineReducers({
     tasks: tasksReducer,
     todolists: todolistsReducer
 })
-
-export const store = createStore(rootReducer)
+//@ts-ignore
+export const store = legacy_createStore(rootReducer, applyMiddleware(thunk))
+ 
+export type AppRootStateType = ReturnType<typeof rootReducer>
+ 
+export type AppThunkDispatch = ThunkDispatch<AppRootStateType, any, AnyAction>
+ 
+export const useAppDispatch = () => useDispatch<AppThunkDispatch>()
+export const useAppSelector: TypedUseSelectorHook<AppRootStateType> = useSelector
