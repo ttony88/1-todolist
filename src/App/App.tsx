6835ffb529e9cb8568ac4b333132bf65c527787d
ttony62 <1988anton62@gmail.com>
2024-04-24 22:46:30 +0300

    update App

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,15 +1,20 @@
-import React, {ChangeEvent, FC, useState}  from 'react'
+import React, { FC, useEffect, useState }  from 'react'
 import style from './App.module.css'
 import { ButtonUsed } from '../components/button-used/ButtonUsed'
-import { TodolistType, addTodolist } from '../redux/todolists-reducer'
+import { FilterType, addTodolist, getTodolist } from '../redux/todolists-reducer'
 import { v1 } from 'uuid'
 import { Todolist } from '../layout/todolist/Todolist'
 import { InputUsed } from '../components/input-used/InputUsed'
 import { useAppDispatch, useAppSelector } from '../redux/store'
+import { TodolistType } from '../API/todolist-api'
 
-export const App:FC = (props) => {
+export const App:FC = () => {
 
     const dispatch = useAppDispatch()
+
+    useEffect(() => {
+        dispatch(getTodolist())
+    }, [])
 
     const[titleTask, setTitleTask] = useState('')
 
@@ -29,7 +34,7 @@ export const App:FC = (props) => {
                 <ButtonUsed textButton="+" onClick={onClickHandler} />
             </div>
             <div className={style.todolists}>
-                {todolists.map((t: TodolistType) => <div key={t.id}><Todolist  
+                {todolists.map((t: TodolistType & {filter: FilterType}) => <div key={t.id}><Todolist  
                                                               title={t.title}
                                                               todolistId={t.id}
                                                               filter={t.filter}/>
