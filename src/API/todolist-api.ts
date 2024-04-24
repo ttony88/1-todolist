3838ae7d28693fe5601a6a8c2f35adc442f6871c
ttony62 <1988anton62@gmail.com>
2024-04-24 22:44:57 +0300

    added todolist-app

---
+++ src/API/todolist-api.ts
@@ -0,0 +1,38 @@
+import axios, { AxiosResponse } from "axios"
+
+export type TodolistType = {
+    id: string
+    addedDate: string
+    order: number
+    title: string
+}
+
+export type ResponseType<D = {}> = {
+    resultCode: number
+    messages: Array<string>
+    fieldsErrors: Array<string>
+    data: D
+}
+
+const instance = axios.create({
+    baseURL: 'https://social-network.samuraijs.com/api/1.1',
+    withCredentials: true
+})
+
+export const todolistAPI = {
+    getTodolist() {
+        return instance.get<TodolistType[]>('/todo-lists')
+    },
+
+    createTodolist(title: string) {
+        return instance.post<ResponseType<{ item: TodolistType }>, AxiosResponse<ResponseType<{ item: TodolistType }>>,{ title: string }>('todo-lists', {title});
+    },
+
+    deleteTodolist(id: string) {
+        return instance.delete<ResponseType>(`todo-lists/${id}`)
+    }, 
+
+    updateTitleTodolist(id: string, title: string) {
+        return instance.put<ResponseType, AxiosResponse<ResponseType>, { title: string }>(`todo-lists/${id}`, {title})
+    }
+}
