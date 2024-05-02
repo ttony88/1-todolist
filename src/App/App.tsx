0807f3d27cd6c57f76f24f3fe264f81d2e961e39
ttony62 <1988anton62@gmail.com>
2024-05-02 22:43:37 +0300

    fixed App

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,7 +1,7 @@
 import React, { FC, useEffect, useState }  from 'react'
 import style from './App.module.css'
 import { ButtonUsed } from '../components/button-used/ButtonUsed'
-import { FilterType, createTodolist, getTodolist } from '../redux/todolists-reducer'
+import { FilterType, createTodolist, getTodolists } from '../redux/todolists-reducer'
 import { Todolist } from '../layout/todolist/Todolist'
 import { InputUsed } from '../components/input-used/InputUsed'
 import { useAppDispatch, useAppSelector } from '../redux/store'
@@ -12,7 +12,7 @@ export const App:FC = () => {
     const dispatch = useAppDispatch()
 
     useEffect(() => {
-        dispatch(getTodolist())
+        dispatch(getTodolists())
     }, [])
 
     const[titleTask, setTitleTask] = useState('')
