dced5783df8adca2f78e480150a9849d552e0d4b
ttony62 <1988anton62@gmail.com>
2024-05-16 00:17:36 +0300

    comment todolists-reduser.test

--- src/redux/todolists-reducer.test.ts
+++ src/redux/todolists-reducer.test.ts
@@ -1,5 +1,5 @@
 // import { v1 } from "uuid"
-// import { StateTodolistType, addTodolist, deleteTodolist, todolistsReducer } from "./todolists-reducer"
+// import { StateTodolistType, addTodolist, removeTodolist, todolistsReducer } from "./todolists-reducer"
 
 // let todolistId1 = v1()
 // let todolistId2 = v1()
@@ -7,20 +7,21 @@
 
 // beforeEach(() => {
 //     startState = [
-//         {id: todolistId1, title: "tools", filter: 'all'},
-//         {id: todolistId2, title: "skills", filter: 'all'},
+//         {id: todolistId1, title: "tools", addedDate: '', order:0, filter: 'all'},
+//         {id: todolistId2, title: "skills", addedDate: '', order:0, filter: 'all'},
 //     ]
 // })
 
 // test("the todolist must be added correctly", () => {
 //     const id = v1()
-//     const endState = todolistsReducer(startState, addTodolist("purchases", id))
+//     const todolist = {id, title: "purchases", addedDate: '', order: 0}
+//     const endState = todolistsReducer(startState, addTodolist(todolist))
 //     expect(endState.length).toBe(3)
 //     expect(endState[0].title).toBe("purchases")
 // })
 
 // test("one of the todolists is being deleted correctly", () => {
-//     const endState = todolistsReducer(startState, deleteTodolist(todolistId1))
+//     const endState = todolistsReducer(startState, removeTodolist(todolistId1))
 //     expect(endState.length).toBe(1)
 //     expect(endState[0].id).toBe(todolistId2)
 // })
