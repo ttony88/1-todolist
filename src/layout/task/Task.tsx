5aadfc5f31033cbe73bf6be27c5e425f1c564fa2
ttony62 <1988anton62@gmail.com>
2024-05-16 00:14:09 +0300

    fixed Tasks

--- src/layout/task/Task.tsx
+++ src/layout/task/Task.tsx
@@ -1,38 +1,57 @@
 import React, {ChangeEvent, FC, useState}  from 'react'
 import style from './Task.module.css'
-import { useDispatch } from 'react-redux'
-import { changeStatusTask, deleteTask } from '../../redux/tasks-reducer'
+import { deleteTask, updateTask } from '../../redux/tasks-reducer'
 import Checkbox from '@mui/material/Checkbox/Checkbox'
 import IconButton from '@mui/material/IconButton/IconButton'
-import { Delete } from '@mui/icons-material'
+import { Delete, Input } from '@mui/icons-material'
 import { useAppDispatch } from '../../redux/store'
 
 type TaskProps = {
     taskId: string
     title: string
-    isDone: boolean
     todolistId: string
 }
 export const Task:FC<TaskProps> = (props) => {
+
+    const [titleValue, setTitleValue] = useState(props.title)
+
+    const [editMode, setEditMode] = useState(false)
     
     const dispatch = useAppDispatch()
 
     const onChangeHandlerCheckBox = (e: ChangeEvent<HTMLInputElement>) => {
-        dispatch(changeStatusTask(props.todolistId, props.taskId, props.isDone))
+        dispatch(updateTask(props.todolistId, props.taskId, {status: 2}))
     }
 
     const onClickHandlerButtonDeleteTask = () => {
         dispatch(deleteTask(props.todolistId, props.taskId))
     }
 
+    const onClickTitleInputHendler = () => {
+        dispatch(updateTask(props.todolistId, props.taskId, {title: titleValue}))
+        setEditMode(false)
+    }
+
+    const onChangeInputTitleHandler = (e: ChangeEvent<HTMLInputElement>) => {
+        setTitleValue(e.currentTarget.value)
+    }
+
+    const onDoubleClickTitleHandler = () => {
+        setEditMode(true)
+    }
+
     return(
         <div className={style.task}> 
-            <Checkbox checked={props.isDone}
-                        defaultChecked={false}
+            <Checkbox checked={true}
+                      defaultChecked={false}
                       onChange={onChangeHandlerCheckBox}
             />
-            <div className={style.title}>
-                {props.title}
+            <div className={style.title} onDoubleClick={onDoubleClickTitleHandler}>
+                {editMode ? <input type='text' 
+                                   value={titleValue} 
+                                   onChange={onChangeInputTitleHandler}
+                                   onClick={onClickTitleInputHendler}
+                                   autoFocus/> : props.title}
             </div>
             <IconButton onClick={onClickHandlerButtonDeleteTask}>
                 <Delete />
