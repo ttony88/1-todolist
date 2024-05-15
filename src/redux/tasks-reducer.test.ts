1881db2ceb89db8f795f72cee852f45ebf7cb861
ttony62 <1988anton62@gmail.com>
2024-05-16 00:15:41 +0300

    comment tasks-reducer.test

--- src/redux/tasks-reducer.test.ts
+++ src/redux/tasks-reducer.test.ts
@@ -1,33 +1,34 @@
-import { TasksStateType, addTask, changeStatusTask, deleteTask, tasksReducer } from "./tasks-reducer";
+// import { TasksStateType, addTask, changeStatusTask, deleteTask, tasksReducer } from "./tasks-reducer";
 
-let startState: TasksStateType
+// let startState: TasksStateType
 
-beforeEach(() => {
-    startState = {
-        "todolistId1": [
-                        {id: "1", title: "React", isDone: false},
-                        {id: "2", title: "Redux", isDone: false},
-                       ],
-        "todolistId2": [
-                        {id: "1", title: "HTML", isDone: false},
-                        {id: "2", title: "CSS", isDone: false},
-                       ],
-    }
-})
+// beforeEach(() => {
+//     startState = {
+//         "todolistId1": [
+//                         {id: "1", title: "React", isDone: false},
+//                         {id: "2", title: "Redux", isDone: false},
+//                        ],
+//         "todolistId2": [
+//                         {id: "1", title: "HTML", isDone: false},
+//                         {id: "2", title: "CSS", isDone: false},
+//                        ],
+//     }
+// })
 
-test("the task must be added correctly", () => {
-    const endState = tasksReducer(startState, addTask("SCSS", "todolistId2"))
-    expect(endState["todolistId2"].length).toBe(3)
-})
+// test("the task must be added correctly", () => {
+//     const endState = tasksReducer(startState, addTask("SCSS", "todolistId2"))
+//     expect(endState["todolistId2"].length).toBe(3)
+// })
 
-test("correct deletion of the task", () => {
-    const action = deleteTask("todolistId1", "1")
-    const endState = tasksReducer(startState, action)
-    expect(endState["todolistId1"].length).toBe(1)
-})
+// test("correct deletion of the task", () => {
+//     const action = deleteTask("todolistId1", "1")
+//     const endState = tasksReducer(startState, action)
+//     expect(endState["todolistId1"].length).toBe(1)
+// })
 
-test("correct status change", () => {
-    const action = changeStatusTask("todolistId1", "1", false)
-    const endState = tasksReducer(startState, action)
-    expect(endState["todolistId1"][1].isDone).toBe(false)
-})
+// test("correct status change", () => {
+//     const action = changeStatusTask("todolistId1", "1", false)
+//     const endState = tasksReducer(startState, action)
+//     expect(endState["todolistId1"][1].isDone).toBe(false)
+// })
+export {}
