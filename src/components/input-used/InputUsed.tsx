4dee8b9a0dc4dc6ff71bffd1a5dd733f0875ab42
ttony62 <1988anton62@gmail.com>
2024-06-25 22:26:51 +0300

    fixed InputUsed

--- src/components/input-used/InputUsed.tsx
+++ src/components/input-used/InputUsed.tsx
@@ -3,15 +3,21 @@ import style from './InputUsed.module.css'
 
 type InputUsedProps = {
     value: string
-    onChange: (value: string) => void
+    type: string
+    name: string
+    onChange: any
 }
 export const InputUsed:FC<InputUsedProps> = (props) => {
 
-    const onChangeHandler = (e: ChangeEvent<HTMLInputElement>) => props.onChange(e.target.value)
+    //const onChangeHandler = (e: ChangeEvent<HTMLInputElement>) => props.onChange(e.target.value)
 
     return(
         <>
-            <input value={props.value} onChange={onChangeHandler} type='text' className={style.input}/>
+            <input value={props.value} 
+                   onChange={props.onChange} 
+                   type={props.type} 
+                   className={style.input}
+                   name={props.name}/>
         </>
     )
 }
